b6579f6e4ad37f06da2690428213bd4ba91d6b9e
josh <jwentwor@gmail.com>
2016-12-22 14:44:22 -0800

    boilerplate untested

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="BG" />
+        <h2>New App</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
